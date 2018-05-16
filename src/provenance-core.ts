a3a402f55ba1a5f89d3a61518bcad72319c8136b
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-05-16 16:45:33 +0200

    Tracker renamed

--- src/provenance-core.ts
+++ src/provenance-core.ts
@@ -1,13 +1,13 @@
 import * as api from './api';
 import { ActionFunctionRegistry } from './ActionFunctionRegistry';
 import { ProvenanceGraph } from './ProvenanceGraph';
-import { ProvenanceGraphTracker } from './ProvenanceGraphTracker';
+import { ProvenanceTracker } from './ProvenanceTracker';
 import { ProvenanceGraphTraverser } from './ProvenanceGraphTraverser';
 
 export {
   api,
   ActionFunctionRegistry,
   ProvenanceGraph,
-  ProvenanceGraphTracker,
+  ProvenanceTracker,
   ProvenanceGraphTraverser
 };
