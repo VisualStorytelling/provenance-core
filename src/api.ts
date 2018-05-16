05e34f10a30dd78365cbd0a1cbe90e0ed3c25f79
Maarten van Meersbergen <maartenvm@gmail.com>
2018-05-16 10:30:20 +0200

    Interface names altered

--- src/api.ts
+++ src/api.ts
@@ -1,70 +1,69 @@
-declare module "API" {
-    export type FunctionName = string;
-    export type Artifacts = { [key: string]: any };
-    export type NodeIdentifier = Number;
-    export type Action = IrreversibleAction | ReversableAction;
 
-    export interface StateNode {
-        id: NodeIdentifier;
-        parent: StateEdge;
-        children: StateEdge[];
-        artifacts: Artifacts;
-    }
+export type FunctionName = string;
+export type Artifacts = { [key: string]: any };
+export type NodeIdentifier = Number;
+export type Action = IIrreversibleAction | IReversableAction;
 
-    export interface StateEdge {
-        previous: StateNode;
-        next: StateNode;
-        action: Action;
-    }
+export type Metadata = {
+    createdBy: string;
+    createdOn: string;
+    tags: string[];
+    userIntent: string;
+    [key: string]: any;
+};
 
-    type Metadata = {
-        createdBy: string;
-        createdOn: string;
-        tags: string[];
-        userIntent: string;
-        [key: string]: any;
-    };
+export interface IStateNode {
+    id: NodeIdentifier;
+    parent: IStateEdge;
+    children: IStateEdge[];
+    artifacts: Artifacts;
+}
 
-    export interface IrreversibleAction {
-        metadata: Metadata;
-        do: FunctionName;
-        doArguments: any[];  // should be immutable
-    }
+export interface IStateEdge {
+    previous: IStateNode;
+    next: IStateNode;
+    action: Action;
+}
 
-    export interface ReversableAction {
-        metadata: Metadata;
-        do: FunctionName;
-        doArguments: any[];  // should be immutable
-        undo: FunctionName;
-        undoArguments: any[]; // should be immutable
-    }
+export interface IIrreversibleAction {
+    metadata: Metadata;
+    do: FunctionName;
+    doArguments: any[];  // should be immutable
+}
+
+export interface IReversableAction {
+    metadata: Metadata;
+    do: FunctionName;
+    doArguments: any[];  // should be immutable
+    undo: FunctionName;
+    undoArguments: any[]; // should be immutable
+}
 
 
-    export interface ProvenanceGraph {
-        version: string;
-        addEdge(edge: StateEdge): void;
-        getStateNode(id: NodeIdentifier): StateNode;
-    }
+export interface IProvenanceGraph {
+    version: string;
+    addEdge(edge: IStateEdge): void;
+    getStateNode(id: NodeIdentifier): IStateNode;
+}
 
-    export interface ProvenanceGraphTracker {
-        current: StateNode;
+export interface IProvenanceGraphTracker {
+    current: IStateNode;
 
-        registerFunction(name: FunctionName, func: Function): void;
+    registerFunction(name: FunctionName, func: Function): void;
 
-        /**
-         * Calls the action.do function with action.doArguments
-         *
-         * @param action
-         *
-         */
-        applyActionToCurrentStateNode(action: Action): Promise<StateNode>;
-        /**
-         * Finds shortest path between current node and node with request identifer.
-         * Calls the do/undo functions of actions on the path.
-         *
-         * @param id
-         */
-        traverseToStateNode(id: NodeIdentifier): StateNode;
+    /**
+     * Calls the action.do function with action.doArguments
+     *
+     * @param action
+     *
+     */
+    applyActionToCurrentStateNode(action: Action): Promise<IStateNode>;
+    /**
+     * Finds shortest path between current node and node with request identifer.
+     * Calls the do/undo functions of actions on the path.
+     *
+     * @param id
+     */
+    traverseToStateNode(id: NodeIdentifier): IStateNode;
 
-    }
 }
