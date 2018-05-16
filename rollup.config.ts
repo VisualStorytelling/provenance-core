ab8a34e88c34f0546c3db2e39e2d55b527b6dbfe
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-05-16 16:12:01 +0200

    Rename package

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'provgraph'
+const libraryName = 'provenance-core'
 
 export default {
   input: `src/${libraryName}.ts`,
