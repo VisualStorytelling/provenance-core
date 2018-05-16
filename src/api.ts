9ef6c79d134a456f58482f9642359d4ca66bd157
Maarten van Meersbergen <maartenvm@gmail.com>
2018-05-16 12:45:15 +0200

    added label and actionResult to StateNode

--- src/api.ts
+++ src/api.ts
@@ -1,81 +1,79 @@
-export type NodeIdentifier = string;
-export type Action = IrreversibleAction | ReversableAction;
+export type NodeIdentifier = string
+export type Action = IrreversibleAction | ReversableAction
 
 export type Metadata = {
-    createdBy: string;
-    createdOn: string;
-    tags: string[];
-    userIntent: string;
-    [key: string]: any;
+  createdBy: string
+  createdOn: string
+  tags: string[]
+  userIntent: string
+  [key: string]: any
 }
 
 export type Artifacts = {
-    [key: string]: any
+  [key: string]: any
 }
 
 export type StateNode = {
-    id: NodeIdentifier;
-    parent: StateEdge | null;
-    children: StateEdge[];
-    artifacts: Artifacts;
+  id: NodeIdentifier
+  label: string
+  actionResult?: any
+  parent: StateEdge | null
+  children: StateEdge[]
+  artifacts: Artifacts
 }
 
 export type StateEdge = {
-    previous: StateNode;
-    next: StateNode;
-    action: Action;
+  previous: StateNode
+  next: StateNode
+  action: Action
 }
 
 export type IrreversibleAction = {
-    metadata: Metadata;
-    do: string;
-    doArguments: any[];  // should be immutable
+  metadata: Metadata
+  do: string
+  doArguments: any[] // should be immutable
 }
 
 export type ReversableAction = {
-    metadata: Metadata;
-    do: string;
-    doArguments: any[];  // should be immutable
-    undo: string;
-    undoArguments: any[]; // should be immutable
+  metadata: Metadata
+  do: string
+  doArguments: any[] // should be immutable
+  undo: string
+  undoArguments: any[] // should be immutable
 }
 
-export type ProvenanceEnabledFunction = (...args: any[]) => Promise<any>;
+export type ProvenanceEnabledFunction = (...args: any[]) => Promise<any>
 
 export interface IProvenanceGraph {
-    version: string;
-    addEdge(edge: StateEdge): void;
-    getStateNode(id: NodeIdentifier): StateNode;
+  version: string
+  addEdge(edge: StateEdge): void
+  getStateNode(id: NodeIdentifier): StateNode
 }
 
 export interface IProvenanceGraphTracker {
-    current: StateNode
+  current: StateNode
 
-   /**
+  /**
    *
    * @param name
    * @param func Function that get called with the doArguments or undoArguments
    *
    */
-    registerFunction(
-        name: string,
-        func: ProvenanceEnabledFunction
-    ): void
+  registerFunction(name: string, func: ProvenanceEnabledFunction): void
 
-    /**
-     * Calls the action.do function with action.doArguments
-     *
-     * @param action
-     *
-     */
-    applyActionToCurrentStateNode(action: Action): Promise<StateNode>
+  /**
+   * Calls the action.do function with action.doArguments
+   *
+   * @param action
+   *
+   */
+  applyActionToCurrentStateNode(action: Action): Promise<StateNode>
 
-    /**
-     * Finds shortest path between current node and node with request identifer.
-     * Calls the do/undo functions of actions on the path.
-     *
-     * @param id
-     */
-    traverseToStateNode(id: NodeIdentifier): Promise<StateNode>
-
+  /**
+   * Finds shortest path between current node and node with request identifer.
+   * Calls the do/undo functions of actions on the path.
+   *
+   * @param id
+   */
+  traverseToStateNode(id: NodeIdentifier): Promise<StateNode>
 }
