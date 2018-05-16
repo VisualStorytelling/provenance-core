c7c4b04f41b78e0395f11907fcc2f2df41885ec3
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-05-16 16:42:23 +0200

    Added library file

---
+++ src/provenance-core.ts
@@ -0,0 +1,13 @@
+import * as api from './api';
+import { ActionFunctionRegistry } from './ActionFunctionRegistry';
+import { ProvenanceGraph } from './ProvenanceGraph';
+import { ProvenanceGraphTracker } from './ProvenanceGraphTracker';
+import { ProvenanceGraphTraverser } from './ProvenanceGraphTraverser';
+
+export {
+  api,
+  ActionFunctionRegistry,
+  ProvenanceGraph,
+  ProvenanceGraphTracker,
+  ProvenanceGraphTraverser
+};
